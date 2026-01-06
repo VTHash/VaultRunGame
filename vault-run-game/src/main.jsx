1f2a6aee32f3e899ab4b14eef5fb03a02f3b28c9
VTHash <hfv@highfinanceventures.com>
2026-01-06 20:21:44 +0000

    Refactor main.jsx

--- vault-run-game/src/main.jsx
+++ vault-run-game/src/main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import "./reown";
 import "./index.css";
 // IMPORTANT: initialize AppKit before rendering App
-import "./reown";
-
+import App from "./App.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
